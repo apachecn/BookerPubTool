557732a928c01b08a2465416423304de62af6b6d
wizardforcel <562826179@qq.com>
2022-07-22 18:47:12 +0800

    2022-07-22 18:47:12

--- BookerPubTool/__main__.py
+++ BookerPubTool/__main__.py
@@ -4,18 +4,18 @@ from . import __version__
 from . import *
 
 def main():
-    parser = argparse.ArgumentParser(prog="PypiBookPublisher", formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(prog="BookerPubTool", formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("-v", "--version", action="version", version=f"PYBP version: {__version__}")
     parser.set_defaults(func=lambda x: parser.print_help())
     subparsers = parser.add_subparsers()
     
-    pub_parser = subparsers.add_parser("publish", help="publish book")
-    pub_parser.add_argument("dir", help="dir")
-    pub_parser.set_defaults(func=publish)
+    pypi_pub_parser = subparsers.add_parser("pub-pypi", help="publish book to pypi")
+    pypi_pub_parser.add_argument("dir", help="dir")
+    pypi_pub_parser.set_defaults(func=publish)
     
-    config_parser = subparsers.add_parser("config", help="configure token")
-    config_parser.add_argument("token", help="token")
-    config_parser.set_defaults(func=config)
+    pypi_config_parser = subparsers.add_parser("conf-pypi", help="configure pypi token")
+    pypi_config_parser.add_argument("token", help="token")
+    pypi_config_parser.set_defaults(func=config)
     
     args = parser.parse_args()
     args.func(args)
