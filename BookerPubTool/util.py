91f7c63d7a3f46d1fe9e1e0f8d6bccb2ecd5fde8
wizardforcel <562826179@qq.com>
2023-02-18 13:44:32 +0800

    2023-02-18 13:44:32

--- BookerPubTool/util.py
+++ BookerPubTool/util.py
@@ -31,19 +31,26 @@ def extname(fname):
     return m.group(1) if m else ''
 
 def gen_proj_name(name):
+    # 转小写并移除扩展名
     name = re.sub(r'\.\w+$', '', name.lower())
-    seg = re.findall(r'[\u4e00-\u9fff]+|[a-zA-Z0-9]+', name)
+    # 提取字母数字和中文，分别处理
+    seg = re.findall(r'[\u4e00-\u9fff]+|[a-z0-9]+', name)
     nseg = []
     p = xpinyin.Pinyin()
     for s in seg:
-        if re.search(r'[a-zA-Z0-9]', s):
+        # 字母数字直接添加
+        # 中文分词之后转拼音
+        if re.search(r'[a-z0-9]', s):
             nseg.append(s)
         else:
             subseg = jieba.cut(s)
             for ss in subseg:
                 nseg.append(p.get_pinyin(ss).replace('-', ''))
     res = '-'.join(nseg)
+    # 数字开头的加上 x 
     if re.search(r'^\d', res): res = 'x' + res
+    # 移除字母数字减号之外的所有字母
+    res = re.sub(r'[^a-z0-9\-]', '', res)
     return res
 
 def npm_filter_name(name):
